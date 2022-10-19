0a3a1fb1a88a5797ad7e2386b794fbcf555f5976
Vincent Mugabo <vicentmugabo75@gmail.com>
2022-10-19 13:36:56 -0700

    Update main

--- main.h
+++ main.h
@@ -4,18 +4,6 @@
 #include <stdlib.h>
 #include <stdarg.h>
 
-
-typedef struct flags
-{
-	int plus;
-	int space;
-	int hash;
-} flags_t;
-
+int _putchar(char c);
 int _printf(const char *format, ...);
-
-
-
-
-
 #endif
