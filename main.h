1d04ce1a1ccca1ad25f7e15432e11fa0b2b0e378
Vincent Mugabo <vicentmugabo75@gmail.com>
2022-10-19 13:29:16 -0700

    Update main

--- main.h
+++ main.h
@@ -1,1 +1,21 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdlib.h>
+#include <stdarg.h>
+
+
+typedef struct flags
+{
+	int plus;
+	int space;
+	int hash;
+} flags_t;
+
 int _printf(const char *format, ...);
+
+
+
+
+
+#endif
